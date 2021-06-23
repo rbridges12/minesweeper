e33411546c8e76491ed276a10bfe76df5ba2393f
Riley Bridges <rlybrdgs@gmail.com>
2021-06-23 16:15:18 -0500

    added recursive blank tile revealing

--- Tile.py
+++ Tile.py
@@ -3,6 +3,7 @@ class Tile:
   def __init__(self):
       self.value = ' '
       self.flagged = False
+      self.revealed = False
 
   def is_mine(self):
      return self.value == 'x'
--- minesweeper.py
+++ minesweeper.py
@@ -2,6 +2,7 @@ from os import write
 import sys
 import random
 import pygame
+from pygame import draw
 import pygame.freetype
 from Tile import Tile
 
@@ -30,99 +31,118 @@ board = [[Tile() for i in range(WIDTH)] for j in range(HEIGHT)]
 
 
 def get_rect(i, j):
-    return pygame.Rect(i*SQUARE_SIZE, j*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE)
+  return pygame.Rect(i*SQUARE_SIZE, j*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE)
 
 
 def write_to_tile(i, j, text, color):
-    text_rect = GAME_FONT.get_rect(text)
-    text_rect.center = get_rect(i, j).center
-    GAME_FONT.render_to(screen, text_rect, text, color)
+  text_rect = GAME_FONT.get_rect(text)
+  text_rect.center = get_rect(i, j).center
+  GAME_FONT.render_to(screen, text_rect, text, color)
 
 
 def on_board(i, j):
-    return i in range(len(board)) and j in range(len(board[0]))
+  return i in range(len(board)) and j in range(len(board[0]))
 
 
 def get_surrounding_tiles(i, j):
-    coords = [
-        (i-1, j-1),
-        (i-1, j),
-        (i-1, j+1),
-        (i, j-1),
-        (i, j+1),
-        (i+1, j-1),
-        (i+1, j),
-        (i+1, j+1)]
+  coords = [
+      (i-1, j-1),
+      (i-1, j),
+      (i-1, j+1),
+      (i, j-1),
+      (i, j+1),
+      (i+1, j-1),
+      (i+1, j),
+      (i+1, j+1)]
 
-    return [board[row][col] for row, col in coords if on_board(row, col)]
+  # return [board[row][col] for row, col in coords if on_board(row, col)]
+  return [coord for coord in coords if on_board(*coord)]
 
 
 def fill_board():
-    for i in range(len(board)):
-        for j in range(len(board[0])):
-            if not board[i][j].is_mine():
-                count = 0
-                for tile in get_surrounding_tiles(i, j):
-                    if tile.is_mine():
-                        count += 1
+  for i in range(len(board)):
+    for j in range(len(board[0])):
+      if not board[i][j].is_mine():
+        count = 0
+        for r, c in get_surrounding_tiles(i, j):
+          if board[r][c].is_mine():
+            count += 1
 
-                if count != 0:
-                    board[i][j].value = str(count)
+        if count != 0:
+          board[i][j].value = str(count)
 
 # TODO: make colors constants
 
 
-def draw_tile(i, j):
-    mines = board[i][j].value
-    if mines == '1':
-        color = (0, 0, 255)
+def draw_hidden_tile(i, j):
+  pygame.draw.rect(screen, BACKGROUND, get_rect(i, j))
+  pygame.draw.rect(screen, BLACK, get_rect(i, j), 1)
 
-    elif mines == '2':
-        color = (0, 255, 0)
 
-    elif mines == '3':
-        color = (255, 255, 0)
+# TODO: store color in class and compute it in prepare board
+def draw_revealed_tile(i, j):
+  mines = board[i][j].value
+  if mines == '1':
+    color = (0, 0, 255)
 
-    elif mines == '4':
-        color = (255, 0, 255)
+  elif mines == '2':
+    color = (0, 255, 0)
 
-    elif mines == '5':
-        color = (255, 0, 0)
+  elif mines == '3':
+    color = (255, 255, 0)
 
-    elif mines == '6':
-        color = (0, 255, 255)
+  elif mines == '4':
+    color = (255, 0, 255)
 
-    elif mines == '7':
-        color = (0, 0, 0)
+  elif mines == '5':
+    color = (255, 0, 0)
 
-    else:
-        color = (128, 128, 128)
+  elif mines == '6':
+    color = (0, 255, 255)
+
+  elif mines == '7':
+    color = (0, 0, 0)
+
+  else:
+    color = (128, 128, 128)
+
+  # make tile white, then write number to it
+  pygame.draw.rect(screen, WHITE, get_rect(i, j))
+  pygame.draw.rect(screen, BLACK, get_rect(i, j), 1)
+  write_to_tile(i, j, mines, color)
+  board[i][j].revealed = True
+
+
+def reveal_tile(i, j):
+  print(i, j)
+  print(board[i][j].value)
+  draw_revealed_tile(i, j)
+  if board[i][j].value == ' ':
+    for r, c in get_surrounding_tiles(i,j):
+      if not board[r][c].revealed:
+        reveal_tile(r, c)
     
-    # make tile white, then write number to it
-    pygame.draw.rect(screen, WHITE, get_rect(i, j))
-    pygame.draw.rect(screen, BLACK, get_rect(i, j), 1)
-    write_to_tile(i, j, mines, color)
-
+    
 
 def mark_mine(i, j):
-    pygame.draw.rect(screen, RED, get_rect(i, j))
+  pygame.draw.rect(screen, RED, get_rect(i, j))
 
 
 # add mines randomly
 mines = NUM_MINES
 while mines > 0:
-    x = random.randrange(0, WIDTH)
-    y = random.randrange(0, HEIGHT)
-    if not board[x][y].is_mine():
-        board[x][y].value = 'x'
-        mines -= 1
+  x = random.randrange(0, WIDTH)
+  y = random.randrange(0, HEIGHT)
+  if not board[x][y].is_mine():
+    board[x][y].value = 'x'
+    mines -= 1
 
 fill_board()
 
 # draw board
 for i in range(len(board)):
-    for j in range(len(board[0])):
-        pygame.draw.rect(screen, BLACK, get_rect(i, j), 1)
+  for j in range(len(board[0])):
+    pygame.draw.rect(screen, BLACK, get_rect(i, j), 1)
 
 
 # GAME_FONT.render_to(screen, (0, 0), 'test', (0, 0, 0))
@@ -133,40 +153,52 @@ for i in range(len(board)):
 
 # game event loop
 while True:
-    pygame.display.flip()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
+  pygame.display.flip()
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      sys.exit()
 
-        elif event.type == pygame.MOUSEBUTTONUP:
-            mouse_location = pygame.mouse.get_pos()
-            button = event.button
+    elif event.type == pygame.MOUSEBUTTONUP:
+      mouse_location = pygame.mouse.get_pos()
+      button = event.button
 
-            for i, row in enumerate(board):
-                for j, tile in enumerate(row):
-                    square = get_rect(i, j)
-                    if square.collidepoint(mouse_location):
-                      
-                        # left click
-                        if button == 1:
+      for i, row in enumerate(board):
+        for j, tile in enumerate(row):
+          square = get_rect(i, j)
+          if square.collidepoint(mouse_location):
 
-                            # mine
-                            if tile.is_mine():
-                                print('you clicked on a mine, you lose')
-                                sys.exit()
+            # left click
+            if button == 1:
+              
+              # left clicking on a flagged tile does nothing
+              if tile.flagged:
+                continue
 
-                            # empty space
-                            elif tile.value == '0':
-                                # reveal then check surrounding tiles to see if they need to be revealed
-                                pass
+              # game over if mine is clicked
+              if tile.is_mine():
+                print('you clicked on a mine, you lose')
+                sys.exit()
 
-                            # space next to mine
-                            else:
-                                print(tile.flagged)
-                                draw_tile(i, j)
+              # # TODO: empty space
+              # elif tile.value == '0':
+              #   # reveal then check surrounding tiles to see if they need to be revealed
+              #   pass
 
-                        # right click
-                        elif button == 3:
-                            board[i][j].flagged = True
-                            print(i, j)
-                            mark_mine(i, j)
+              # space next to mine
+              else:
+                reveal_tile(i, j)
+
+            # right click
+            elif button == 3:
+              
+              # you can only flag tiles that arent revealed
+              if not tile.revealed:
+                
+                # right clicking on an already flagged tile unflags it
+                if tile.flagged:
+                  tile.flagged = False
+                  draw_hidden_tile(i, j)
+                  
+                else:
+                  tile.flagged = True
+                  mark_mine(i, j)
