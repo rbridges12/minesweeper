54d69fcb80a22f92edcb316f68cbb49d2b49d812
Riley Bridges <rlybrdgs@gmail.com>
2021-09-03 16:11:43 -0400

    fixed some flag logic

--- minesweeper.py
+++ minesweeper.py
@@ -2,16 +2,19 @@
 import sys
 import random
 import pygame
-from pygame import draw
+from pygame import draw, init
 import pygame.freetype
 from Tile import Tile
 
-# TODO: make a class - use member variables for flag and revealed count
 # TODO: mine generation at first click to make sure you start on an empty space
+#       - record tile that was clicked on
+#       - generate random position of mines
+#       - regenerate position if it lands on a previous mine
+#         or in a 3x3 box around the tile that was clicked
 # TODO: buttons to start/new game instead of quitting the whole program
+# TODO: display flag counter on screen
 # TODO: adjust board size and mine density in game
 # TODO: make it prettier
-# TODO: flag counter
 # TODO: you lose/you win message on screen instead of print
 # TODO: time to completion
 # TODO: time leaderboard
@@ -43,7 +46,7 @@ class Minesweeper:
     # init board
     self.board = [[Tile() for i in range(self.HEIGHT)]
                   for j in range(self.WIDTH)]
-    
+
     # counters
     self.flags_used = 0
     self.revealed_tiles = 0
@@ -73,7 +76,7 @@ class Minesweeper:
     # return [board[row][col] for row, col in coords if on_board(row, col)]
     return [coord for coord in coords if self.on_board(*coord)]
 
-  def fill_board(self):
+  def fill_tile_values(self):
     for i in range(len(self.board)):
       for j in range(len(self.board[0])):
         if not self.board[i][j].is_mine():
@@ -128,29 +131,34 @@ class Minesweeper:
   def reveal_tile(self, i, j):
     self.draw_revealed_tile(i, j)
     self.revealed_tiles += 1
-    
+
     # if an empty space was revealed, recursively reveal all surrounding tiles
+    # do not reveal flagged tiles
     if self.board[i][j].value == ' ':
       for r, c in self.get_surrounding_tiles(i, j):
-        if not self.board[r][c].revealed:
+        if not self.board[r][c].revealed and not self.board[r][c].flagged:
+          print(self.board[r][c].value)
           self.reveal_tile(r, c)
 
   def draw_flagged_tile(self, i, j):
     pygame.draw.rect(self.screen, self.RED, self.get_rect(i, j))
     pygame.draw.rect(self.screen, self.BLACK, self.get_rect(i, j), 1)
 
-  def init_game(self):
+  def place_mines(self, initial_tile):
+    # add mines randomly, avoiding the 3x3 area around where the player first clicked
+    starting_tiles = self.get_surrounding_tiles(*initial_tile)
 
-    # add mines randomly
     mines = self.NUM_MINES
     while mines > 0:
       x = random.randrange(0, self.WIDTH)
       y = random.randrange(0, self.HEIGHT)
-      if not self.board[x][y].is_mine():
+      if not self.board[x][y].is_mine() and not (x, y) in starting_tiles:
         self.board[x][y].value = 'x'
         mines -= 1
 
-    self.fill_board()
+  def init_game(self):
+    self.place_mines((4, 4))
+    self.fill_tile_values()
 
     # draw board
     for i in range(len(self.board)):
@@ -210,12 +218,13 @@ class Minesweeper:
 
           print(f"revealed tiles: {self.revealed_tiles}")
           print(f"flags used: {self.flags_used}")
+
           # if all possible tiles have been revealed, you win
           if self.revealed_tiles == (self.WIDTH * self.HEIGHT) - self.NUM_MINES:
             print('you win!')
             sys.exit()
-            
-            
+
+
 game = Minesweeper()
 game.init_game()
 game.run_game()
